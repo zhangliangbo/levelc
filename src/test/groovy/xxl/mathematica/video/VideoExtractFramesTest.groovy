d6fc19ea1c6a38e71724030e0079db7c56231d87
zhangliangbo <2472110501@qq.com>
2020-06-11 13:09:12 +0800

    显示视频的帧

--- src/test/groovy/xxl/mathematica/video/VideoExtractFramesTest.groovy
+++ src/test/groovy/xxl/mathematica/video/VideoExtractFramesTest.groovy
@@ -2,24 +2,24 @@ package xxl.mathematica.video
 
 import io.vavr.Tuple2
 import org.bytedeco.javacv.Frame
+import xxl.mathematica.image.ShowImage
+import xxl.mathematica.list.Range
 
 import javax.imageio.ImageIO
-import java.util.function.Consumer
+import java.util.function.Function
 
 class VideoExtractFramesTest extends GroovyTestCase {
     void testVideoExtractFrames() {
-        io.vavr.collection.List.ofAll(VideoExtractFrames.videoExtractFrames("D:\\big.mp4", [0, 1, 2]))
+        def files = io.vavr.collection.List.ofAll(VideoExtractFrames.videoExtractFrames("D:\\big.mp4", Range.range(999, 1009)))
                 .zipWithIndex()
-                .forEach(new Consumer<Tuple2<Frame, Integer>>() {
+                .map(new Function<Tuple2<Frame, Integer>, String>() {
                     @Override
-                    void accept(Tuple2<Frame, Integer> tuple2) {
-                        try {
-                            ImageIO.write(EnhanceFrameUtils.toBufferedImage(tuple2._1), "jpg", new File("D:\\pet" + tuple2._2 + ".jpg"));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    String apply(Tuple2<Frame, Integer> tuple2) {
+                        String file = "D:\\pet" + tuple2._2 + ".jpg"
+                        ImageIO.write(EnhanceFrameUtils.toBufferedImage(tuple2._1), "jpg", new File(file))
+                        return file
                     }
-                })
-
+                }).asJava()
+        ShowImage.showImage(files as String[])
     }
 }
