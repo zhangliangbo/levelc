6ff85b4225354cc42dc98aa0e75f808ecb399b66
zhangliangbo <2472110501@qq.com>
2020-06-11 13:38:17 +0800

    显示视频的帧

--- README.md
+++ README.md
@@ -1,21 +1,4 @@
-# 和c/c++交互
+# 和c/c++交互，实际效果参考https://blog.csdn.net/ssehs/article/details/106686615
 # 音视频
 ## VideoExtractFrames 提取视频的指定位置的帧
-```
-void testVideoExtractFrames() {
-    def files = io.vavr.collection.List.ofAll(VideoExtractFrames.videoExtractFrames("D:\\big.mp4", [333, 666, 999]))
-            .zipWithIndex()
-            .map(new Function<Tuple2<Frame, Integer>, String>() {
-                @Override
-                String apply(Tuple2<Frame, Integer> tuple2) {
-                    String file = "D:\\pet" + tuple2._2 + ".jpg"
-                    ImageIO.write(EnhanceFrameUtils.toBufferedImage(tuple2._1), "jpg", new File(file))
-                    return file
-                }
-            }).asJava()
-    ShowImage.showImage(files as String[])
-}
-
-
-```
 # 图像处理
--- src/test/groovy/xxl/mathematica/video/VideoFrameListTest.groovy
+++ src/test/groovy/xxl/mathematica/video/VideoFrameListTest.groovy
@@ -1,24 +1,23 @@
 package xxl.mathematica.video
 
-
 import org.bytedeco.javacv.Frame
 import xxl.mathematica.Rule
+import xxl.mathematica.image.ShowImage
 
 import javax.imageio.ImageIO
-import java.util.function.Consumer
+import java.util.function.Function
 
 class VideoFrameListTest extends GroovyTestCase {
     void testVideoFrameList() {
-        io.vavr.collection.List.ofAll(VideoFrameList.videoFrameList("D:\\big.mp4", 10, false))
-                .forEach(new Consumer<Rule<Integer, Frame>>() {
+        def files = io.vavr.collection.List.ofAll(VideoFrameList.videoFrameList("D:\\big.mp4", 3, false))
+                .map(new Function<Rule<Integer, Frame>, String>() {
                     @Override
-                    void accept(Rule<Integer, Frame> rule) {
-                        try {
-                            ImageIO.write(EnhanceFrameUtils.toBufferedImage(rule.value), "jpg", new File("D:\\big" + rule.getKey() + ".jpg"));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    String apply(Rule<Integer, Frame> rule) {
+                        String file = "D:\\big" + rule.getKey() + ".jpg"
+                        ImageIO.write(EnhanceFrameUtils.toBufferedImage(rule.value), "jpg", new File(file))
+                        return file
                     }
-                })
+                }).asJava()
+        ShowImage.showImage(files as String[])
     }
 }
