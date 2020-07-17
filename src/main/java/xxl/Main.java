3627b6e007cd34af477ed5534d3f8dd237150bc8
zhangliangbo <2472110501@qq.com>
2020-07-17 09:01:03 +0800

    视频帧转发

--- src/main/java/xxl/Main.java
+++ src/main/java/xxl/Main.java
@@ -1,7 +1,9 @@
 package xxl;
 
+import xxl.mathematica.video.VideoDevice;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        System.err.println(VideoDevice.videoDevice());
     }
 }
